5d6bc2067424ee2469d54171b31cb3c2aaf173fd
Ilham Wahabi <wahabiputra@gmail.com>
2019-06-15 12:05:17 +0700

    Remove unused stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
       }
     },
     `gatsby-plugin-postcss`,
