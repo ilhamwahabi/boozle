5f77517b71391dea99d2bab760d48cbef3ec7cae
Steven Sukma <steven.sukma.limanus@gmail.com>
2019-06-15 23:24:59 +0700

    update color

--- src/components/BookingButton.tsx
+++ src/components/BookingButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const BookingButton = () => {
   return (
-    <button className="bg-orange-400 px-8 py-2 text-xl text-white rounded-sm">
+    <button className="bg-primary px-8 py-2 text-xl text-white rounded-sm">
       <Link to="/search">Reserve Venues</Link>
     </button>
   );
