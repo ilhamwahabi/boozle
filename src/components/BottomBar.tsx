98a7e17b165b8112f00385757f773dff3f4fba3f
Ilham Wahabi <wahabiputra@gmail.com>
2019-06-15 18:25:19 +0700

    Able navigate with bottom bar

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -19,25 +19,34 @@ const BottomBar = () => {
         fixed left-0 bottom-0 p-4 bg-white w-screen
       "
     >
-      <BottomBarItem icon={faHome} label="Home" selected={pathname === "/"} />
+      <BottomBarItem
+        icon={faHome}
+        label="Home"
+        path="/"
+        selected={pathname === "/"}
+      />
       <BottomBarItem
         icon={faHistory}
         label="History"
+        path="/history"
         selected={pathname === "/history"}
       />
       <BottomBarItem
         icon={faChartBar}
         label="Statistic"
+        to="/statistic"
         selected={pathname === "/statistic"}
       />
       <BottomBarItem
         icon={faFutbol}
         label="Match"
+        to="/match"
         selected={pathname === "/match"}
       />
       <BottomBarItem
         icon={faUser}
         label="Profile"
+        to="/profile"
         selected={pathname === "/profile"}
       />
     </div>
